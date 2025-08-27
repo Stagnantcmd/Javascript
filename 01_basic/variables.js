0deb3806178645f191e89a881918c14757ed8298
Stagnantcmd <armanpreet633@gmail.com>
2025-08-27 09:24:51 +0000

    variables

---
+++ 01_basic/variables.js
@@ -0,0 +1,15 @@
+let StuId=23;
+var StuName="Ankit";
+let city="jaipur";
+let age=49;
+
+Stu_marks;
+
+//Not prefer to use variable name without any variable
+
+
+
+//console.table --> print output in table format
+console.table([StuId,StuName,city,age]);
+
+//always prefer let over var because issue of block scope
---
+++ 01_basic/variables2.js
@@ -0,0 +1,25 @@
+"use strict"  //this allow to use javascript on basis of newer version
+
+//alert("hello")  // Not work because we are using node not browser
+
+
+let Student="rakesh"
+let age=22
+let attendance=true
+let marks=null
+let salary;
+//int 
+//big int
+//boolean=> true/false
+//null => no value
+//undefined=> value not assign
+//string => ""
+//symbol=> unique
+
+
+//object
+
+
+//typeof
+
+console.table([typeof Student,typeof age,typeof attendance,typeof marks])
