74a96b27a5d26771e851c4ea29226d6b65e20297
Stagnantcmd <armanpreet633@gmail.com>
2025-08-30 09:41:47 +0000

    dates

--- 01_basic/dates.js
+++ 01_basic/dates.js
@@ -12,5 +12,20 @@ let date=new Date()
 
 let newdate=new Date(2025,3,5,6,7)
 //date with time
-console.log(newdate);
-console.log(newdate.toLocaleString());
+// console.log(newdate);
+// console.log(newdate.toLocaleString());
+
+let dates= Date.now()
+
+// console.log(dates)
+
+// console.log(Math.floor(Date.now()/1000))
+
+
+let datess= new Date(2023,0,1)
+console.log(datess.getMonth()+1)
+
+datess.toLocaleString('Default',{
+    weekday:"long"
+})
+//use t0 customize object
