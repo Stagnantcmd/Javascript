407aafcbc0d67ee0c79ca465879a61aba0c34de6
Stagnantcmd <armanpreet633@gmail.com>
2025-08-30 08:29:31 +0000

    load

---
+++ 01_basic/dates.js
@@ -0,0 +1,16 @@
+let date=new Date()
+// console.log(date)
+// console.log(date.toString())
+// console.log(date.toISOString())
+// console.log(date.toJSON())
+// console.log(date.toLocaleDateString())
+// console.log(date.toLocaleString())
+// console.log(date.toTimeString())
+
+
+//date is object type
+
+let newdate=new Date(2025,3,5,6,7)
+//date with time
+console.log(newdate);
+console.log(newdate.toLocaleString());
