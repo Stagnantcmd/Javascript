7f659637800c0d9ac8b2a5ef8cc6f0dce720e6be
Stagnantcmd <armanpreet633@gmail.com>
2025-09-05 16:19:34 +0000

    for

---
+++ 04_flow/for.js
@@ -0,0 +1,10 @@
+for (let i = 0; i < 10; i++) {
+     const element =i;
+     console.log(element)
+     for (let j = 0; j < 10; j++) {
+        const element1 = j;
+        console.log(element1)
+        
+    }
+ 
+}
